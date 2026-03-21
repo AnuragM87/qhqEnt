0f98251d5d8a238b69756a0eca8b33515c836b69
Anurag Chaudhary <anuragchaudharyM87@gmail.com>
2026-03-21 15:29:14 +0530

    adde fidelity methods

--- src/fidelity.py
+++ src/fidelity.py
@@ -96,3 +96,34 @@ def uhlmann_fidelity(rho, sigma, eps=1e-12):
 
     F = (np.sum(np.sqrt(eigvals)))**2
     return float(np.clip(F, 0.0, 1.0))
+
+
+
+    # import numpy as np
+
+# def pure_state_fidelity(rho_exp, rho_target):
+#     """
+#     Fidelity between an experimental state and a target PURE state.
+
+#     Parameters
+#     ----------
+#     rho_exp : density matrix (NxN)
+#     rho_target : pure state density matrix (NxN)
+
+#     Returns
+#     -------
+#     Fidelity (float)
+#     """
+
+#     # Ensure Hermitian
+#     rho_exp = (rho_exp + rho_exp.conj().T) / 2
+#     rho_target = (rho_target + rho_target.conj().T) / 2
+
+#     # Normalize traces
+#     rho_exp = rho_exp / np.trace(rho_exp)
+#     rho_target = rho_target / np.trace(rho_target)
+
+#     # Fidelity: Tr(rho_target * rho_exp)
+#     F = np.trace(rho_target @ rho_exp)
+
+#     return float(np.real(F))
